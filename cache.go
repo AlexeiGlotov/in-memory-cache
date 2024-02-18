3ab1bc21b2397e9797729db2bbfd09998f6848bc
AlexeiGlotov <tnffx@icloud.com>
2024-02-18 19:00:07 +0300

    fix race condition

--- cache.go
+++ cache.go
@@ -1,4 +1,4 @@
-package in_memory_cache_
+package in_memory_cache
 
 import (
 	"sync"
@@ -13,21 +13,25 @@ type ElemValue struct {
 
 type MyCache struct {
 	cache map[string]ElemValue
-	mu    sync.Mutex
+	mu    *sync.Mutex
 }
 
 func NewCache() *MyCache {
 	return &MyCache{
 		cache: make(map[string]ElemValue),
+		mu:    new(sync.Mutex),
 	}
 }
 
-func (m MyCache) Set(key string, value interface{}, ttl time.Duration) {
+func (m *MyCache) Set(key string, value interface{}, ttl time.Duration) {
+	m.mu.Lock()
 	m.cache[key] = ElemValue{value: value, ttl: ttl, timeCreate: time.Now()}
+	m.mu.Unlock()
 }
 
 func (m MyCache) Get(key string) (res ElemValue, value interface{}) {
-
+	m.mu.Lock()
+	defer m.mu.Unlock()
 	val, ok := m.cache[key]
 	if ok {
 		if time.Since(m.cache[key].timeCreate) > m.cache[key].ttl {
@@ -43,12 +47,14 @@ func (m MyCache) Get(key string) (res ElemValue, value interface{}) {
 }
 
 func (m MyCache) Delete(key string) {
+	m.mu.Lock()
 	_, ok := m.cache[key]
 	if ok {
 		delete(m.cache, key)
 	}
+	m.mu.Unlock()
 }
 
-func (m MyCache) Info() (res map[string]ElemValue) {
+func (m MyCache) Info() map[string]ElemValue {
 	return m.cache
 }
